fcc8ce3f33de89898810b663b3fbc15890041bfa
hieunhangia <hieuvippro531@gmail.com>
2026-01-25 23:51:21 +0700

    update product card to use min/max price range and rename product image property.

--- Frontend/src/types/products/product.ts
+++ Frontend/src/types/products/product.ts
@@ -2,7 +2,7 @@ export interface ProductSummary {
   id: string;
   name: string;
   slug: string;
-  price: number;
-  salePrice?: number | null;
-  imageUrl: string;
+  minPrice: number;
+  maxPrice: number;
+  mainImageUrl: string;
 }
