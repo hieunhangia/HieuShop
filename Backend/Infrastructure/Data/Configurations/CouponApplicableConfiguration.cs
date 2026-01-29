671a856097669d51fb6172cb2cda7bc9141d0aae
hieunhangia <hieuvippro531@gmail.com>
2026-01-29 20:02:14 +0700

    remove redundant configurations from CouponApplicableConfiguration

--- Backend/Infrastructure/Data/Configurations/CouponApplicableConfiguration.cs
+++ Backend/Infrastructure/Data/Configurations/CouponApplicableConfiguration.cs
@@ -1,5 +1,4 @@
 using Domain.Entities.Coupons;
-using Domain.Entities.Products;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -19,38 +18,5 @@ public class CouponApplicableConfiguration : IEntityTypeConfiguration<CouponAppl
             .HasValue<CouponApplicableProduct>("Product")
             .HasValue<CouponApplicableCategory>("Category")
             .HasValue<CouponApplicableBrand>("Brand");
-    }
-}
-
-public class CouponApplicableProductConfiguration : IEntityTypeConfiguration<CouponApplicableProduct>
-{
-    public void Configure(EntityTypeBuilder<CouponApplicableProduct> builder)
-    {
-        builder.HasOne<Product>()
-            .WithMany()
-            .HasForeignKey(cap => cap.ProductId)
-            .OnDelete(DeleteBehavior.Restrict);
-    }
-}
-
-public class CouponApplicableCategoryConfiguration : IEntityTypeConfiguration<CouponApplicableCategory>
-{
-    public void Configure(EntityTypeBuilder<CouponApplicableCategory> builder)
-    {
-        builder.HasOne<Category>()
-            .WithMany()
-            .HasForeignKey(cac => cac.CategoryId)
-            .OnDelete(DeleteBehavior.Restrict);
-    }
-}
-
-public class CouponApplicableBrandConfiguration : IEntityTypeConfiguration<CouponApplicableBrand>
-{
-    public void Configure(EntityTypeBuilder<CouponApplicableBrand> builder)
-    {
-        builder.HasOne<Brand>()
-            .WithMany()
-            .HasForeignKey(cab => cab.BrandId)
-            .OnDelete(DeleteBehavior.Restrict);
     }
 }
